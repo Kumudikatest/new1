2376e8bf2f2c13a8e9ce442b45ea36cd3ae31c72
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-22 13:54:29 +0530

    add 2 cognito with get and enable ope

--- new1/lambda.js
+++ new1/lambda.js
@@ -12,6 +12,28 @@ exports.handler = function (event, context, callback) {
         }
         // your logic goes within this block
     });
+    cognito_idp.adminGetUser({
+        UserPoolId: process.env.UserPoolId_cognitoKNew, /* required */
+        Username: "Test" /* required */
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+    cognito_idp.adminEnableUser({
+        UserPoolId: process.env.UserPoolId_cognitoKNew, /* required */
+        Username: "Test" /* required */
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+
 
     callback(null, { "message": "Successfully executed" });
 }
