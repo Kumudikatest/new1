c3c3d6f98e413e5619be6414aa62fcd25703af3b
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-22 13:49:13 +0530

    add a new cognito pool

---
+++ new1/lambda.js
@@ -0,0 +1,17 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitoKNew,
+        Limit: "10"
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
